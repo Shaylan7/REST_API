75d06c96b555f68bc63b3517191f94baa2afa8c7
Shaylan Hawthorne <shaylan.hawthorne@gmail.com>
2021-08-20 09:58:51 -0700

    Initial Commit

--- book-api.js
+++ book-api.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-// used for parsing the json 
 const cors = require('cors');
-// request is coming from another domain
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-
-
-// Where we will keep books
-let books = [];
+let books = [{
+    "isbn": "9780582330870",
+    "title": "Animal Farm",
+    "author": "George Orwell",
+    "publish_date": "1945-17-8",
+    "publisher": "Simon & Schuster",
+    "numOfPages": 140,
+},
+{
+    "isbn": "9780345392824",
+    "title": "Reviving Ophelia: saving the selves of adolescent girls",
+    "author": "Mary Pipher",
+    "publish_date": "1994-07-01",
+    "publisher": "Ballantine Books",
+    "numOfPages": 320,
+},
+{
+    "isbn": "9780316285254",
+    "title": "David and Goliath: underdogs, misfits, and the art of battling giants",
+    "author": "Malcom Gladwell",
+    "publish_date": "2015-01-04",
+    "publisher": "Back Bay Books",
+    "numOfPages": 352,
+}];
 
 app.use(cors());
 
@@ -20,11 +38,66 @@ app.use(bodyParser.json());
 app.post('/book', (req, res) => {
     const book = req.body;
 
+    // output the book to the console for debugging
     console.log(book);
     books.push(book);
 
     res.send('Book is added to the database');
-
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.get('/book', (req, res) => {
+    res.json(books);
+});
+
+app.get('/book/:isbn', (req, res) => {
+    // reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // searching books for the isbn
+    for (let book of books) {
+        if (book.isbn === isbn) {
+            res.json(book);
+            return;
+        }
+    }
+
+    // sending 404 when not found something is a good practice
+    res.status(404).send('Book not found');
+});
+
+app.delete('/book/:isbn', (req, res) => {
+    // reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // remove item from the books array
+    books = books.filter(i => {
+        if (i.isbn !== isbn) {
+            return true;
+        }
+
+        return false;
+    });
+
+    // sending 404 when not found something is a good practice
+    res.send('Book is deleted');
+});
+
+app.post('/book/:isbn', (req, res) => {
+    // reading isbn from the URL
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+
+    // remove item from the books array
+    for (let i = 0; i < books.length; i++) {
+        let book = books[i]
+
+        if (book.isbn === isbn) {
+            books[i] = newBook;
+        }
+    }
+
+    // sending 404 when not found something is a good practice
+    res.send('Book is edited');
+});
+
+app.listen(port, () => console.log(`Hello book app listening on port ${port}!`));
